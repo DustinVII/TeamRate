2a4a3aa2e0fd99c206f20a4e34bf71577200a6a5
DustinVII <dustinrr@gmail.com>
2024-06-27 16:23:07 -0300

    Pushed

--- com/teamrate/Main.java
+++ com/teamrate/Main.java
@@ -1,9 +1,20 @@
 package com.teamrate;
 
+import com.teamrate.database.DatabaseManager;
+
+import java.sql.Connection;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        Connection connection = DatabaseManager.getConnection();
+        if (connection == null) {
+            System.err.println("Connection could not be established.");
+        } else {
+            System.out.println("");
+        }
 
 
     }
