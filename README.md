1f5c84fd8e2ef8d9ac7d611ded5e767360bc0331
DustinVII <dustinrr@gmail.com>
2024-06-26 23:05:04 -0300

    Pushed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TeamRate
+
+Test
---
+++ com/teamrate/Main.java
@@ -0,0 +1,11 @@
+package com.teamrate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+    }
+
+}
